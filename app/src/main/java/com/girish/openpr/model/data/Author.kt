b48c03c0aa134642ed20637c2afbcc826b5e903f
Girish Budhwani <gibudhwa@microsoft.com>
2019-06-07 15:47:00 +0530

    data model changes

--- app/src/main/java/com/girish/openpr/model/data/Author.kt
+++ app/src/main/java/com/girish/openpr/model/data/Author.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class Author(
     @SerializedName("login") val username : String,
-    @SerializedName("html_url") val pageUrl : String,
+    @SerializedName("html_url") val profilePageUrl : String,
     @SerializedName("avatar_url") val avatarUrl : String
 )
--- app/src/main/java/com/girish/openpr/model/data/PullRequest.kt
+++ app/src/main/java/com/girish/openpr/model/data/PullRequest.kt
@@ -5,8 +5,10 @@ import com.google.gson.annotations.SerializedName
 // TODO: add other relevant fields.
 data class PullRequest(
     @SerializedName("user") val author: Author,
-    @SerializedName("body") val body: String,
     @SerializedName("title") val title: String,
+    @SerializedName("body") val body: String,
+    @SerializedName("html_url") val prPageUrl: String,
+    @SerializedName("number") val prNumber: String,
     // TODO: time format?
     @SerializedName("created_at") val createdOn : String
 )
