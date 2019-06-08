2085ec2695535c3f9369dd7a840e7b8ba59ac6ef
Girish Budhwani <gibudhwa@microsoft.com>
2019-06-08 13:14:17 +0530

    removed redundant field

--- app/src/main/java/com/girish/openpr/model/data/PullRequest.kt
+++ app/src/main/java/com/girish/openpr/model/data/PullRequest.kt
@@ -2,13 +2,10 @@ package com.girish.openpr.model.data
 
 import com.google.gson.annotations.SerializedName
 
-// TODO: add other relevant fields.
 data class PullRequest(
     @SerializedName("user") val author: Author,
     @SerializedName("title") val title: String,
     @SerializedName("body") val body: String,
     @SerializedName("html_url") val prPageUrl: String,
     @SerializedName("number") val prNumber: String,
-    // TODO: time format?
-    @SerializedName("created_at") val createdOn : String
 )
